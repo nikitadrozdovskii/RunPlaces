71e63f3600503f32ed20fac45b9af1e0f8e9c7d6
nikitadrozdovskii <butch386@gmail.com>
2018-10-02 12:03:26 -0500

    added Run and Place interfaces, added dummy list of places to home page

---
+++ src/interfaces/run.ts
@@ -0,0 +1,8 @@
+import {Place} from './place';
+
+export class Run{
+    constructor(public date: string, 
+                public distance: number,
+                public pace: number,
+                public places: Place[] ){}
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ApplicationRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Run } from '../../interfaces/run';
+import { Place } from '../../interfaces/place';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  request;
+  service;
+  map;
+  runs: Run[] =[];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private appRef: ApplicationRef) {
+    this.runs = [new Run("10/2/2018",2.9,460,[new Place("Oxford square"),new Place("Grove")]),
+    new Run("9/30/2018",3,480,[new Place("Faulkner's house"),new Place("Grove")]),
+    new Run("9/27/2018",2,500,[new Place("Lyceum"),new Place("Lamar Park")])]
   }
+
 
 }
